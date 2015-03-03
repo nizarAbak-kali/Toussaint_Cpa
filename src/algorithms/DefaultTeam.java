6603debd06e22a8630fd3b6b98bdc692d29a7f30
Nizar Abak-kali <nizarabakkali93@gmail.com>
2015-03-03 18:00:08 +0100

    ajout de Ritter et Main pour run

--- src/algorithms/DefaultTeam.java
+++ src/algorithms/DefaultTeam.java
@@ -1,218 +1,37 @@
 package algorithms;
 
-import java.awt.Point;
-import java.util.ArrayList;
-
-/***************************************************************
- * TME 1: calcul de diamètre et de cercle couvrant minimum.    *
- *   - Trouver deux points les plus éloignés d'un ensemble de  *
- *     points donné en entrée.                                 *
- *   - Couvrir l'ensemble de poitns donné en entrée par un     *
- *     cercle de rayon minimum.                                *
- *                                                             *
- * class Circle:                                               *
- *   - Circle(Point c, int r) constructs a new circle          *
- *     centered at c with radius r.                            *
- *   - Point getCenter() returns the center point.             *
- *   - int getRadius() returns the circle radius.              *
- *                                                             *
- * class Line:                                                 *
- *   - Line(Point p, Point q) constructs a new line            *
- *     starting at p ending at q.                              *
- *   - Point getP() returns one of the two end points.         *
- *   - Point getQ() returns the other end point.               *
- ***************************************************************/
-
 import supportGUI.Circle;
 import supportGUI.Line;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+
 public class DefaultTeam {
-	//TRI PAR PIXEL POUR GRAHAM
-	// on trie les point 
-	public ArrayList<Point> filtrealacon(ArrayList<Point> points) {
-		//contient les points dans l'orde croissant de leurs abscisses
-		ArrayList<Point> bucket_abc = new ArrayList<Point>();
-		//contient les points dans l'orde croissant de leurs abscisses
-		ArrayList<Point> bucket_ord = new ArrayList<Point>();
-		ArrayList<Point> inter = new ArrayList<Point>();
-		
-		
-		//on remplit les array avant de les trier
-		for(Point i : points){
-			bucket_abc.add(i);
-			bucket_ord.add(i);
-			
-		}
-		
-		boolean echange = true;
-		//tri a bulle sur les abscisses
-		Point tmp , a , b ;
-		b = a = tmp = null;
-		int j = bucket_abc.size();
-		while((j>0) && echange){
-			echange = false ;
-			//peut etre -1
-			for (int i = 1 ;i < bucket_abc.size(); i++){
-				a = bucket_abc.get(i);
-				b = bucket_abc.get(i+1);
-				if(a.x > b.x){
-					tmp = a;
-					a = b ;
-					b = tmp;
-					echange = true;
-				}//fin si 		
-			}//fin for 
-			j = j-1;
-		}//fin while
-		
-		j = bucket_ord.size();
-		//tri pour les ordonnées 
-		while((j>0) && echange){
-			echange = false ;
-			//peut etre -1
-			for (int i = 1 ;i < bucket_ord.size(); i++){
-				a = bucket_ord.get(i);
-				b = bucket_ord.get(i+1);
-				if(a.y > b.y){
-					tmp = a;
-					a = b ;
-					b = tmp;
-					echange = true;
-				}//fin si 		
-			}//fin for 
-			j = j-1;
-		}//fin while
-		
-		//on retire les point de meme abscisses
-		for (int i = 1 ;i < bucket_abc.size()-1; i++){
-			a = bucket_abc.get(i-1);
-			tmp = bucket_abc.get(i);
-			b = bucket_abc.get(i+1);
-			
-			if ((a.x==tmp.x) && (b.x==tmp.x)){
-				bucket_abc.remove(i);
-				i--;
-			}
-		}
-		for (int i = 1 ;i < bucket_ord.size()-1; i++){
-			a = bucket_ord.get(i-1);
-			tmp = bucket_ord.get(i);
-			b = bucket_ord.get(i+1);
-			
-			if ((a.x==tmp.x) && (b.x==tmp.x)){
-				bucket_ord.remove(i);
-				i--;
-			}
-		}
-		 for(Point p :bucket_abc)
-			 for(Point q:bucket_ord)
-				 if(p.equals(q))
-					 inter.add(p);
-		
-		return 	inter;
-	}
-	
-	public ArrayList<Point> filtreParPixel(ArrayList<Point> points){
-		ArrayList<Point> res = new ArrayList<Point>();
-		ArrayList<Point> copie = new ArrayList<Point>();
-		copie = (ArrayList<Point>) points.clone();
-		 int ordmax , ordmin ,ordmoy;
-		 ordmax = 0 ;
-		 ordmin = points.get(0).y;
-		 
-		 
-		 for(Point p :points){
-			 if(ordmax < p.y)
-				 ordmax = p.y;
-			 if(ordmin > p.y)
-				 ordmin = p.y;
-		 }
-		ordmoy = (ordmax+ordmin)/2 ;
-		
-		ArrayList<Point> petit = new ArrayList<Point>();
-		ArrayList<Point> grand = new ArrayList<Point>();
-		
-		for(Point p : points){
-			if(p.y<= ordmoy)
-				petit.add(p);
-			else {
-				grand.add(p);
-			}	
-		}
-		// on trie les deux sous listes 
-		  
-		
-		return res ;
-	}
+    //TRI PAR PIXEL POUR GRAHAM
+    // on trie les point
+    public Line filtrealacon(ArrayList<Point> points) {
+        //contient les points dans l'orde croissant de leurs abscisses
+        ArrayList<Point> bucket_abc = new ArrayList<Point>();
+        //contient les points dans l'orde croissant de leurs abscisses
+        ArrayList<Point> bucket_ord = new ArrayList<Point>();
+        ArrayList<Point> inter = new ArrayList<Point>();
 
-	// calculDiametre: ArrayList<Point> --> Line
-	//   renvoie une pair de points de la liste, de distance maximum.
-	public Line calculDiametre(ArrayList<Point> points) {
-		if (points.size()<3) {
-			return null;
-		}
-		  Point ptmp1 = new Point();
-	        Point ptmp2 = new Point();
-	        int distmax, tmp;
-	        distmax = 0;
-	        tmp = 0;
 
-	        for (Point p : points) {
-	            for (Point q : points) {
-	                if(p.equals(q)) continue;
-	                tmp = (int) (p.distance(q) + 0.5) ;
-	                if (distmax < tmp) {
-	                    distmax = tmp;
-	                    ptmp1.x = p.x;
-	                    ptmp1.y = p.y;
-	                    ptmp2.x = q.x;
-	                    ptmp2.y = q.y;
+        //on remplit les array avant de les trier
+        for (Point i : points) {
+            bucket_abc.add(i);
+            bucket_ord.add(i);
 
-	                }
-	            }
-	        }
-	        
-	        
-	        
-	        return new Line(ptmp1, ptmp2);
-	
-	}
-	//SHAMOS VRAI 
-	// calculDiametreOptimise: ArrayList<Point> --> Line
-		//   renvoie une pair de points de la liste, de distance maximum.
-		public Line calculDiametreOptimise(ArrayList<Point> points) {
-			if (points.size()<3) {
-				return null;
-			}
-			Point a = points.get(0);
-			Point b = points.get(1);
-			
-			//calculer le point p de  dist max de la droite (ab)
-			
-			Point p = points.get(0);
-			for(Point tmp : points){
-				
-			}
-			
-			/*******************
-			 * PARTIE A ECRIRE *
-			 *******************/
-			return new Line(a,b);
-		}
-	
-	// calculCercleMin: ArrayList<Point> --> Circle
-	//   renvoie un cercle couvrant tout point de la liste, de rayon minimum.
-	public Circle calculCercleMin(ArrayList<Point> points) {
-		if (points.isEmpty()) {
-			return null;
-		}
+        }
 
-		Point center=points.get(0);
-		int radius=100;
-		
-		/*******************
-		 * PARTIE A ECRIRE *
-		 *******************/
+        Point center = points.get(0);
+        int radius = 100;
+
+        /*******************
+         * PARTIE A ECRIRE *
+         *******************/
         Point ptmp1 = new Point();
         Point ptmp2 = new Point();
         int distmax, tmp;
@@ -221,8 +40,72 @@ public class DefaultTeam {
 
         for (Point p : points) {
             for (Point q : points) {
-                if(p.equals(q)) continue;
-                tmp = (int) (p.distance(q) + 0.5) ;
+                if (p.equals(q)) continue;
+                tmp = (int) (p.distance(q) + 0.5);
+                if (distmax < tmp) {
+                    distmax = tmp;
+                    ptmp1.x = p.x;
+                    ptmp1.y = p.y;
+                    ptmp2.x = q.x;
+                    ptmp2.y = q.y;
+
+                }
+            }
+        }
+
+
+        return new Line(ptmp1, ptmp2);
+
+    }
+
+
+    //SHAMOS VRAI
+    // calculDiametreOptimise: ArrayList<Point> --> Line
+    //   renvoie une pair de points de la liste, de distance maximum.
+    public Line calculDiametreOptimise(ArrayList<Point> points) {
+        if (points.size() < 3) {
+            return null;
+        }
+        Point a = points.get(0);
+        Point b = points.get(1);
+
+        //calculer le point p de  dist max de la droite (ab)
+
+        Point p = points.get(0);
+        for (Point tmp : points) {
+
+        }
+
+        /*******************
+         * PARTIE A ECRIRE *
+         *******************/
+        return new Line(a, b);
+    }
+
+    // calculCercleMin: ArrayList<Point> --> Circle
+    //   renvoie un cercle couvrant tout point de la liste, de rayon minimum.
+    //verision perso et naive
+    public Circle calculCercleMinNaif(ArrayList<Point> points) {
+        if (points.isEmpty()) {
+            return null;
+        }
+
+        Point center = points.get(0);
+        int radius = 100;
+
+        /*******************
+         * PARTIE A ECRIRE *
+         *******************/
+        Point ptmp1 = new Point();
+        Point ptmp2 = new Point();
+        int distmax, tmp;
+        distmax = 0;
+        tmp = 0;
+
+        for (Point p : points) {
+            for (Point q : points) {
+                if (p.equals(q)) continue;
+                tmp = (int) (p.distance(q) + 0.5);
                 if (distmax < tmp) {
                     distmax = tmp;
                     ptmp1.x = p.x;
@@ -239,31 +122,112 @@ public class DefaultTeam {
         radius = (int) (ptmp1.distance(ptmp2)) / 2;
 
 
-		return new Circle(center,radius);
-	}
-	
-	// enveloppeConvexe: ArrayList<Point> --> ArrayList<Point>
-	//   renvoie l'enveloppe convexe de la liste.
-	//GRAHAM
-	public ArrayList<Point> enveloppeConvexe(ArrayList<Point> points){
-		if (points.size()<3) {
-			return null;
-		}
-		
-		ArrayList<Point> enveloppe = new ArrayList<Point>();
-		
-		enveloppe.add(points.get(0));
-		enveloppe.add(points.get(1));
-		enveloppe.add(points.get(2));
-		
-		
-		/*******************
-		 * PARTIE A ECRIRE *
-		 *******************/
-		
-		
-		return points;
-	}
-	
+        return new Circle(center, radius);
+    }
+
+    // enveloppeConvexe: ArrayList<Point> --> ArrayList<Point>
+    //   renvoie l'enveloppe convexe de la liste.
+    public ArrayList<Point> enveloppeConvexe(ArrayList<Point> points) {
+        if (points.size() < 3) {
+            return null;
+        }
+
+        ArrayList<Point> enveloppe = new ArrayList<Point>();
+
+        enveloppe.add(points.get(0));
+        enveloppe.add(points.get(1));
+        enveloppe.add(points.get(2));
+
+
+        /*******************
+         * PARTIE A ECRIRE *
+         *******************/
+
+
+        //return points;
+        return enveloppe;
+    }
+
+
+    /**
+     * *****************************     PROJET CPA    ******************************************
+     */
+    //RITTER pour le cercle min       FAIT et sa TOURNE
+    public Circle calculCercleMin(ArrayList<Point> points) {
+        if (points.isEmpty()) {
+            return null;
+        }
+        Point P = null, Q = null;
+        // 1- Choisir un point dummy appartenant a Points
+        Point dummy = points.get(0);
+        double max = Integer.MIN_VALUE;
+        double distancePQ = Integer.MIN_VALUE;
+        // 2- Trouver P le point le plus eloigne du point dummy
+        for (Point i : points) {
+            if (dummy.distance(i) >= max) {
+                max = dummy.distance(i);
+                P = i;
+            }
+        }
+        // 3- Trouver Q le point le plus eloigne du point P
+        for (Point i : points) {
+            if (P.distance(i) >= distancePQ) {
+                distancePQ = P.distance(i);
+                Q = i;
+            }
+        }
+        // 4- Trouver le point centrePQ
+        Point centrePQ = new Point((P.x + Q.x) / 2, (P.y + Q.y) / 2);
+        // 5- Le cercle ayant le point centre pour centre, et passant par P et Q
+        double rayon_c = P.distance(Q) / 2;
+        Circle c = new Circle(centrePQ, (int) rayon_c);
+        // 6- Enlever les points couverts par le cercle c
+        Circle cp = null;
+        Circle ctemp = c;
+        Point temp = centrePQ;
+        double rtemp = rayon_c;
+        ArrayList<Point> pointsOut = new ArrayList<Point>();
+        do {
+            pointsOut = null;
+            pointsOut = new ArrayList<Point>();
+            // La liste pointsOut contient tous les points qui ne sont pas couvert par c.
+            for (Point i : points) {
+                if (temp.distance(i) > rtemp) {
+                    pointsOut.add(i);
+                }
+            }
+
+            System.out.println(pointsOut.size());
+            // A la fin de cette boucle si pointsOut est vide => c est le cercle minimum !
+            if (pointsOut.isEmpty()) return ctemp;
+            // 7- s un point parmi ceux qui ne sont pas couverts par le cercle c.
+            Point s = pointsOut.get((new Random()).nextInt(pointsOut.size()));
+            // 8-
+
+            double centrePQ_s = temp.distance(s); // cs
+            double cps = (rtemp + centrePQ_s) / 2; // le nouveau rayon !
+            double ccp = centrePQ_s - cps;
+
+            // 9- Le point centre est le centre du segment ST
+            Point centre = new Point((int) ((cps / centrePQ_s) * temp.x + (ccp / centrePQ_s) * s.x),
+                    (int) ((cps / centrePQ_s) * temp.y + (ccp / centrePQ_s) * s.y));
+
+            // 10- cp est le cercle ayant centre pour centre est cps pour rayon.
+            cp = new Circle(centre, (int) centre.distance(s));
+            temp = centre;
+            ctemp = cp;
+
+            rtemp = centre.distance(s);
+
+
+        } while (!pointsOut.isEmpty());
+
+
+        return ctemp;
+    }
+
+
+    /***************************************************************************/
+
 
 }
---
+++ src/algorithms/Main.java
@@ -0,0 +1,113 @@
+package algorithms;
+
+import supportGUI.FramedDiamRace;
+import supportGUI.Variables;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * Created by nizar on 03/03/15.
+ */
+public class Main {
+    private static int width = 0;
+    private static int height = 0;
+    private static String title = "Diameter Racer";
+    private static String filename = "input.points";
+    private static FramedDiamRace framedGUI;
+    private static int nbPoints = 10000;
+
+
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        label50:
+        while (true) {
+            if (i >= args.length) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        framedGUI = new FramedDiamRace(width, height, title, nbPoints);
+                    }
+                });
+                Object var7 = Variables.lock;
+                synchronized (Variables.lock) {
+                    try {
+                        Variables.lock.wait();
+                    } catch (InterruptedException var4) {
+                        var4.printStackTrace();
+                    }
+                }
+
+                readFile();
+                return;
+            }
+
+            if (args[i].charAt(0) == 45) {
+                if (args[i + 1].charAt(0) == 45) {
+                    System.err.println("Option " + args[i] + " expects an argument but received none");
+                    return;
+                }
+
+                String e1;
+                switch ((e1 = args[i]).hashCode()) {
+                    case -2118335772:
+                        if (e1.equals("-nbPoints")) {
+                            try {
+                                nbPoints = Integer.parseInt(args[i + 1]);
+                            } catch (Exception var6) {
+                                System.err.println("Invalid argument for option " + args[i] + ": Integer type expected");
+                                return;
+                            }
+
+                            ++i;
+                            break;
+                        }
+                    default:
+                        break label50;
+                }
+            }
+
+            ++i;
+        }
+
+        System.err.println("Unknown option " + args[i]);
+    }
+
+    public static void readFile() {
+        ArrayList points = new ArrayList();
+
+        try {
+            BufferedReader e = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
+
+            try {
+                String line;
+                while ((line = e.readLine()) != null) {
+                    String[] coordinates = line.split("\\s+");
+                    points.add(new Point(Integer.parseInt(coordinates[0]), Integer.parseInt(coordinates[1])));
+                }
+
+                framedGUI.drawPoints(points);
+                Object e1 = Variables.lock2;
+                synchronized (Variables.lock2) {
+                    Variables.lock2.notify();
+                }
+            } catch (IOException var16) {
+                System.err.println("Exception: interrupted I/O.");
+            } finally {
+                try {
+                    e.close();
+                } catch (IOException var14) {
+                    System.err.println("I/O exception: unable to close " + filename);
+                }
+
+            }
+        } catch (FileNotFoundException var18) {
+            System.err.println("Input file not found.");
+        }
+
+    }
+}
+
